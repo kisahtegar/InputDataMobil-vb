4bc583ab42cb6f02c4f3de1a4e9c7d6a09ce6b3c
kisah <data.kisahtegar@gmail.com>
2023-05-28 20:09:49 +0700

    Add project files.

---
+++ InputDataMobil/InputDataMobil.Designer.vb
@@ -0,0 +1,220 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class InputDataMobil
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.tbType = New System.Windows.Forms.TextBox()
+        Me.tbHarga = New System.Windows.Forms.TextBox()
+        Me.cbNama = New System.Windows.Forms.ComboBox()
+        Me.cbWarna = New System.Windows.Forms.ComboBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnKeluar = New System.Windows.Forms.Button()
+        Me.btnSimpan = New System.Windows.Forms.Button()
+        Me.btnTambah = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.OleDbCommand1 = New System.Data.OleDb.OleDbCommand()
+        Me.OleDbConnection1 = New System.Data.OleDb.OleDbConnection()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(101, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(223, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "INPUT DATA MOBIL"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 78)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(90, 18)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Type Mobil"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(12, 131)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(98, 18)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Nama Mobil"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(12, 180)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(53, 18)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Harga"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(12, 234)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(103, 18)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Warna Mobil"
+        '
+        'tbType
+        '
+        Me.tbType.Location = New System.Drawing.Point(206, 79)
+        Me.tbType.Name = "tbType"
+        Me.tbType.Size = New System.Drawing.Size(205, 20)
+        Me.tbType.TabIndex = 5
+        '
+        'tbHarga
+        '
+        Me.tbHarga.Location = New System.Drawing.Point(206, 181)
+        Me.tbHarga.Name = "tbHarga"
+        Me.tbHarga.Size = New System.Drawing.Size(205, 20)
+        Me.tbHarga.TabIndex = 6
+        '
+        'cbNama
+        '
+        Me.cbNama.FormattingEnabled = True
+        Me.cbNama.Items.AddRange(New Object() {"Avanza", "Pajero", "Lamborghini", "Ferari"})
+        Me.cbNama.Location = New System.Drawing.Point(206, 131)
+        Me.cbNama.Name = "cbNama"
+        Me.cbNama.Size = New System.Drawing.Size(205, 21)
+        Me.cbNama.TabIndex = 7
+        '
+        'cbWarna
+        '
+        Me.cbWarna.FormattingEnabled = True
+        Me.cbWarna.Items.AddRange(New Object() {"Merah", "Kuning", "Hijau", "Biru"})
+        Me.cbWarna.Location = New System.Drawing.Point(206, 235)
+        Me.cbWarna.Name = "cbWarna"
+        Me.cbWarna.Size = New System.Drawing.Size(205, 21)
+        Me.cbWarna.TabIndex = 8
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnKeluar)
+        Me.GroupBox1.Controls.Add(Me.btnSimpan)
+        Me.GroupBox1.Controls.Add(Me.btnTambah)
+        Me.GroupBox1.Location = New System.Drawing.Point(92, 272)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(248, 102)
+        Me.GroupBox1.TabIndex = 9
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Toolbox Operasi"
+        '
+        'btnKeluar
+        '
+        Me.btnKeluar.Location = New System.Drawing.Point(168, 41)
+        Me.btnKeluar.Name = "btnKeluar"
+        Me.btnKeluar.Size = New System.Drawing.Size(75, 23)
+        Me.btnKeluar.TabIndex = 2
+        Me.btnKeluar.Text = "Keluar"
+        Me.btnKeluar.UseVisualStyleBackColor = True
+        '
+        'btnSimpan
+        '
+        Me.btnSimpan.Location = New System.Drawing.Point(87, 40)
+        Me.btnSimpan.Name = "btnSimpan"
+        Me.btnSimpan.Size = New System.Drawing.Size(75, 23)
+        Me.btnSimpan.TabIndex = 1
+        Me.btnSimpan.Text = "Simpan"
+        Me.btnSimpan.UseVisualStyleBackColor = True
+        '
+        'btnTambah
+        '
+        Me.btnTambah.Location = New System.Drawing.Point(6, 41)
+        Me.btnTambah.Name = "btnTambah"
+        Me.btnTambah.Size = New System.Drawing.Size(75, 23)
+        Me.btnTambah.TabIndex = 0
+        Me.btnTambah.Text = "Tambah"
+        Me.btnTambah.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(3, 380)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(417, 203)
+        Me.DataGridView1.TabIndex = 10
+        '
+        'OleDbConnection1
+        '
+        Me.OleDbConnection1.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\MyData\Project\cpp\Project\Input" &
+    "DataMobil\database\dbmobil.mdb"
+        '
+        'InputDataMobil
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(423, 595)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.cbWarna)
+        Me.Controls.Add(Me.cbNama)
+        Me.Controls.Add(Me.tbHarga)
+        Me.Controls.Add(Me.tbType)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "InputDataMobil"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents tbType As System.Windows.Forms.TextBox
+    Friend WithEvents tbHarga As System.Windows.Forms.TextBox
+    Friend WithEvents cbNama As System.Windows.Forms.ComboBox
+    Friend WithEvents cbWarna As System.Windows.Forms.ComboBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents btnKeluar As System.Windows.Forms.Button
+    Friend WithEvents btnSimpan As System.Windows.Forms.Button
+    Friend WithEvents btnTambah As System.Windows.Forms.Button
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents OleDbCommand1 As System.Data.OleDb.OleDbCommand
+    Friend WithEvents OleDbConnection1 As System.Data.OleDb.OleDbConnection
+
+End Class
---
+++ InputDataMobil/InputDataMobil.vb
@@ -0,0 +1,89 @@
+﻿Imports System.Data.OleDb
+
+Public Class InputDataMobil
+    Dim conn As OleDbConnection
+    Dim da As OleDbDataAdapter
+    Dim ds As DataSet
+    Dim cmd As OleDbCommand
+    Dim str As String
+
+    ' METHOD: Koneksi()
+    Sub Koneksi()
+        ' Ganti path str sesuai dengan lokasi Connection String database.
+        str = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\MyData\Project\cpp\Project\InputDataMobil\database\dbmobil.mdb"
+        conn = New OleDbConnection(str)
+        If conn.State = ConnectionState.Closed Then conn.Open()
+    End Sub
+
+    ' METHOD: Tampilgrid()
+    Sub Tampilgrid()
+        da = New OleDbDataAdapter("select * from tb_mobil", conn)
+        ds = New DataSet
+        da.Fill(ds, "tb_mobil")
+        DataGridView1.DataSource = ds.Tables("tb_mobil")
+    End Sub
+
+    ' METHOD: InputDataMobil_Load()
+    Private Sub InputDataMobil_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Call Koneksi()
+        Call Tampilgrid()
+        Call TextMati()
+        Me.btnTambah.Enabled = True
+        Me.btnSimpan.Enabled = False
+        Me.btnKeluar.Enabled = True
+    End Sub
+
+    ' METHOD: TextMati()
+    Sub TextMati()
+        Me.tbType.Enabled = False
+        Me.tbHarga.Enabled = False
+        Me.cbNama.Enabled = False
+        Me.cbWarna.Enabled = False
+    End Sub
+
+    ' METHOD: TextHidup()
+    Sub TextHidup()
+        Me.tbType.Enabled = True
+        Me.tbHarga.Enabled = True
+        Me.cbNama.Enabled = True
+        Me.cbWarna.Enabled = True
+    End Sub
+
+    ' METHOD: Kosong()
+    Sub Kosong()
+        tbType.Clear()
+        tbHarga.Clear()
+    End Sub
+
+    ' BUTTON: Button Tambah
+    Private Sub btnTambah_Click(sender As Object, e As EventArgs) Handles btnTambah.Click
+        Call Kosong()
+        Call TextHidup()
+        Me.btnTambah.Enabled = False
+        Me.btnSimpan.Enabled = True
+        Me.btnKeluar.Enabled = True
+    End Sub
+
+    ' BUTTON: Button Simpan
+    Private Sub btnSimpan_Click(sender As Object, e As EventArgs) Handles btnSimpan.Click
+        If tbType.Text = "" Or cbNama.Text = "" Or tbHarga.Text = "" Or cbWarna.Text = "" Then
+            MsgBox("Data belum lengkap, pastikan semua form terisi")
+            Exit Sub
+        Else
+            Call Koneksi()
+            Dim simpan As String = "insert into tb_mobil (typeMobil, namaMobil, hargaMobil, warnaMobil)" & "values ('" & tbType.Text & "','" & cbNama.Text & "','" & tbHarga.Text & "','" & cbWarna.Text & "')"
+            cmd = New OleDbCommand(simpan, conn)
+            cmd.ExecuteNonQuery()
+            MsgBox("Data berhasil di input", MsgBoxStyle.Information, "Information")
+            Me.OleDbConnection1.Close()
+            Call Tampilgrid()
+            DataGridView1.Refresh()
+            Call Koneksi()
+            Call Kosong()
+            Call TextMati()
+            Me.btnTambah.Enabled = True
+            Me.btnSimpan.Enabled = False
+            Me.btnKeluar.Enabled = True
+        End If
+    End Sub
+End Class
